be20443e7c23d0fcf85a5bef6ceaa79e341683df
Joe Dytrych <j@dytry.ch>
2016-03-01 15:25:06 +0000

    Update nvim install

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ set -x
 
 ln -sf $PWD/zshrc ~/.zshrc
 
-mkdir -p ~/.config
+mkdir -p config/nvim/bundle
+mkdir -p config/nvim/_backup
+mkdir -p config/nvim/_temp
 cd config/nvim/bundle
 git clone https://github.com/Shougo/neobundle.vim.git
 cd -
+mkdir -p ~/.config
 ln -sf $PWD/config/nvim ~/.config/nvim

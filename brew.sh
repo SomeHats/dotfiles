8ba1f835eb41234d4264a8d913ef4c217bd717e7
Joe Dytrych <j@dytry.ch>
2016-03-01 15:02:46 +0000

    Add brew things to install

---
+++ brew.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+brew install go --with-cc-common
+brew install chruby md5shasum node
+brew install neovim/neovim/neovim
+brew install elasticsearch
+brew install postgres
+brew install direnv
+brew install macvim
+brew install openssl
+brew install pkg-config
+brew install ossp-uuid
+brew install python
+brew install python3
+brew install s3cmd
+brew install coreutils
+
